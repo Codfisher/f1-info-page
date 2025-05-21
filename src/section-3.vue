e58cc30fb12e96c034d3333002f8309fcfe8be34
Codfisher <pony2140@livemail.tw>
2025-05-22 00:24:56 +0800

    Add new car images: car-3.png and car-4.png to assets

--- src/section-3.vue
+++ src/section-3.vue
@@ -4,8 +4,8 @@
       The Unpredictable Elite: Top Teams’ Albert Park Mastery (1996-2024)
     </div>
 
-    <div class="">
-      <f1-tabs :tabs></f1-tabs>
+    <div class="w-full ">
+      <f1-tabs class=" w-full"></f1-tabs>
 
     </div>
   </div>
@@ -14,23 +14,6 @@
 <script setup lang="ts">
 import F1Tabs from './components/f1-tabs.vue';
 
-const tabs = [
-  {
-    title: 'Ferrari',
-    description: 'Ferrari has been a dominant force in F1, especially at Albert Park.',
-    years: '1996-2024',
-  },
-  {
-    title: 'Mercedes',
-    description: 'Mercedes has shown consistent performance at Albert Park.',
-    years: '2014-2024',
-  },
-  {
-    title: 'Red Bull Racing',
-    description: 'Red Bull Racing has made significant strides at Albert Park.',
-    years: '2009-2024',
-  },
-];
 </script>
 
 <style scoped lang="sass">
