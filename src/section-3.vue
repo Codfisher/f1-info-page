4b448066380b4f3873e749fcab55305e66554e90
Codfisher <pony2140@livemail.tw>
2025-05-22 00:45:48 +0800

    chore: update dependencies and improve section-3.vue layout
    - Added @types/d3 for TypeScript support.
    - Updated d3 to version 7.9.0.
    - Added animejs version 4.0.2.
    - Adjusted f1-tabs component usage in section-3.vue for cleaner syntax.

--- src/section-3.vue
+++ src/section-3.vue
@@ -2,14 +2,13 @@
   <div class="flex flex-col flex-nowrap items-center bg-[#222] p-10 gap-4 text-base text-white">
     <div class="  text-3xl ">
       The Unpredictable Elite: Top Teams’ Albert Park Mastery (1996-2024)
-      {{ tab }}
     </div>
 
     <div class="w-full ">
       <f1-tabs
         v-model="tab"
         class=" w-full"
-      ></f1-tabs>
+      />
 
     </div>
   </div>
