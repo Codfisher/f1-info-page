40eda197e5101c319e5094c44507f5854dadbc1b
Codfisher <pony2140@livemail.tw>
2025-05-23 00:20:22 +0800

    feat: 更新 f1-step-chart 組件的顯示邏輯，新增過渡效果並根據視窗大小調整高度

--- src/section-3.vue
+++ src/section-3.vue
@@ -22,10 +22,19 @@
         </div>
       </div>
 
-      <f1-step-chart
-        :key="teamName"
-        :team-name="teamName"
-      />
+      <div
+        class=" relative"
+        :style="{ height: `${windowSize.height / 2}px` }"
+      >
+        <transition name="opacity">
+          <f1-step-chart
+            :key="chartKey"
+            :team-name="teamName"
+            :year-range="yearRange"
+            class=" absolute"
+          />
+        </transition>
+      </div>
 
       <f1-period-tabs
         v-model="period"
@@ -36,14 +45,41 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, reactive, ref, watch } from 'vue';
 import F1PeriodTabs, { Period } from './components/f1-period-tabs.vue';
 import F1StepChart from './components/f1-step-chart.vue';
 import { teamList } from './constants';
+import { useWindowSize } from '@vueuse/core';
+
+const windowSize = reactive(useWindowSize())
+
+
+const periodYearRangeMap: Record<
+  Period, [number, number]
+> = {
+  'Raw Power': [1996, 2000],
+  'Aero Revolution': [2001, 2009],
+  'Pre-Hybrid': [2010, 2013],
+  'Hybrid Domination': [2014, 2021],
+  'Ground Era': [2022, 2024],
+}
 
 const period = ref<Period | ''>('')
+const yearRange = computed(() => {
+  if (period.value === '') return
+  return periodYearRangeMap[period.value]
+})
 
 const teamName = ref<string>()
+
+const chartKey = ref(0)
+watch(
+  () => [teamName.value, yearRange.value],
+  () => {
+    chartKey.value += 1
+  },
+  { deep: true }
+)
 </script>
 
 <style scoped lang="sass">
