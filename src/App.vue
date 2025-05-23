15770ce7856ce66b85e84aca58f2b38c3c2e899a
Codfisher <pony2140@livemail.tw>
2025-05-24 01:58:30 +0800

    feat: 更新 App.vue，新增 F1 賽事介紹內容以增強頁面信息

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="flex flex-col">
-    <section-1 class="min-h-[100vh] bg-[#222] text-white" />
-    <section-2 class="bg-[#111] text-white" />
-    <section-3 class="min-h-[100vh] bg-[#222] text-white" />
+  <div class="flex flex-col bg-[#222] text-white">
+    <div class="p-10">
+      <div class=" text-4xl text-center p-4">
+        F1 MELBOURNE: THE MOST UNPREDICTABLE RACE
+      </div>
+
+      <div class="">
+        As Formula 1's traditional season opener, Melbourne's Albert Park Circuit sets a chaotic tone for the
+        championship. This poster examines how top constructors, Ferrari, Mercedes, Red Bull and McLaren, fluctuate
+        dramatically at this
+        unpredictable track compared to their seasonal performance. It reveals how Albert Park's unique challenges have
+        exposed team vulnerabilities across different regulatory eras, from the V10 period to today's ground effect cars
+      </div>
+    </div>
+
+    <section-1 class="min-h-[100vh]  " />
+    <section-2 class="bg-[#111] " />
+    <section-3 class="min-h-[100vh]  " />
   </div>
 </template>
 
